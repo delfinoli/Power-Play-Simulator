1baa367badf5f8e008b3948cf045dd5fc6ac059d
fino-li <101760181+fino-li@users.noreply.github.com>
2023-05-14 21:58:50 -0700

    created junction

---
+++ junction.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var clickPos = Vector2(0,0)
+var canDraw = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	clickPos = Vector2(position.x, position.y)
+	pass # Replace with function body.
+
+func _draw():
+	if canDraw == true:
+		draw_circle(Vector2(0,0),15,Color(255,0,0))
+
+func _on_body_entered(body):
+	canDraw = true
+	if Input.is_action_just_pressed("leftClick"):
+		clickPos = get_local_mouse_position()
+	if global.atTarget == true and global.hasCone == true:
+		global.hasCone = false
+		global.score += 5
+		queue_redraw()
