b324ae84dcc2ca93251efc1acb7c21ac83c02c6d
fino-li <101760181+fino-li@users.noreply.github.com>
2023-05-14 21:59:05 -0700

    added global variables to communicate between scenes

---
+++ global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var score = 0
+var hasCone = false
+var substationCones = 20
+var inSubstation = false
+
+var atTarget = true
+
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
