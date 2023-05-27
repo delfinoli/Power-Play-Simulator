84f1dc9c070a685ab700f6296639f4a2263815f4
fino-li <101760181+fino-li@users.noreply.github.com>
2023-05-27 12:30:02 -0700

    Fixed bug where you can't score if already in junction area

--- junction.gd
+++ junction.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var clickPos = Vector2(0,0)
 var canDraw = false
+var inJunction = false
+var currentBody
 
 var redOwns
 
@@ -20,10 +22,39 @@ func _ready():
 func _draw():
 	if canDraw == true:
 		draw_circle(Vector2(0,0),10,coneColor)
+		
+func _process(delta):
+	if inJunction == true:
+		if Input.is_action_just_pressed("leftClick"):
+			clickPos = get_local_mouse_position()
+		if currentBody.clickPos.distance_to(position) <= 20 and currentBody.hasCone == true and currentBody.inSubstation == false:
+			currentBody.hasCone = false
+			if currentBody.red == true:
+				global.redScore += score
+				coneColor = Color(255,0,0)
+				if redOwns == null:
+					global.redScore +=3
+				elif redOwns == false:
+					global.redScore += 3
+					global.blueScore -= 3
+				redOwns = true
+			else:
+				global.blueScore += score
+				coneColor = Color(0,0,255)
+				if redOwns == null:
+					global.blueScore +=3
+				elif redOwns == true:
+					global.redScore -= 3
+					global.blueScore += 3
+				redOwns = false
+			queue_redraw()
 
 func _on_body_entered(body):
 	if body.name == "Robot":
 		canDraw = true
+		inJunction = true
+		currentBody = body
+		print(inJunction)
 		if Input.is_action_just_pressed("leftClick"):
 			clickPos = get_local_mouse_position()
 		if body.clickPos.distance_to(position) <= 20 and body.hasCone == true and body.inSubstation == false:
@@ -47,3 +78,8 @@ func _on_body_entered(body):
 					global.blueScore += 3
 				redOwns = false
 			queue_redraw()
+	
+
+func _on_body_exited(body):
+	inJunction = false
+	print(inJunction)
